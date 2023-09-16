d827c81ccd1772b8cd1bf25c3a9eb9ef99e844fb
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2023-09-16 00:28:19 -0500

    import of .obj files (vertices only) achieved

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # https://en.wikipedia.org/wiki/Projection_(linear_algebra)
 # https://en.wikipedia.org/wiki/3D_projection
 # https://en.wikipedia.org/wiki/Painter%27s_algorithm
+# https://free3d.com/3d-model/low-poly-male-26691.html
 from dataclasses import dataclass
 from typing import Tuple, List
 from math import sin, cos
@@ -28,23 +29,26 @@ FOV = 1000
 
 screen_size = (1280, 720)
 view_distance = 640*8
-vertex_size = 15
+vertex_size = 2
 
-camera = [0, 0, -1000]
+camera = [0, 15, -50]
 scale = 100
 separation = 200
 
 
 pygame.init()
-pygame.display.set_caption('interpolacion_lineal')
+pygame.display.set_caption('obj')
 
 # CMUSerif = pygame.font.Font("cmunbx.ttf", 32)
 CMUSerif = pygame.font.Font("cmunbx.ttf", 64)
-orthogonal_text = CMUSerif.render("Interpolación lineal", True, (255, 255, 255))
+orthogonal_text = CMUSerif.render("Importación de archivos OBJ", True, (255, 255, 255))
+solovertices_text = CMUSerif.render("(sólo vértices)", True, (255, 255, 255))
 orthogonal_text_rect = orthogonal_text.get_rect()
-orthogonal_text_rect.center = (screen_size[0]//2, screen_size[1]//6)
+orthogonal_text_rect.center = (screen_size[0]//2, screen_size[1]//12)
+solovertices_text_rect = solovertices_text.get_rect()
+solovertices_text_rect.center = (screen_size[0]//2, screen_size[1]//5)
 
-orthogonal_matrix_image = pygame.image.load("lerp.png")
+# orthogonal_matrix_image = pygame.image.load("lerp.png")
 # orthogonal_matrix_image = pygame.transform.scale(orthogonal_matrix_image, (orthogonal_matrix_image.get_width()*9//10,
 #                                                                            orthogonal_matrix_image.get_height()*9//10))
 
@@ -89,7 +93,7 @@ class V(object):
         
         size = min(max(0, (-vertex_size/view_distance)*(self.z-camera[2]) + vertex_size), vertex_size)
 
-        print(self.project(camera), self.brightness(camera), size)
+        # print(self.project(camera), self.brightness(camera), size)
         pygame.draw.circle(screen,
                            (self.brightness(camera),
                             self.brightness(camera), 
@@ -97,29 +101,21 @@ class V(object):
                            self.project(camera),
                            size)
 
-cube = [
-    V(-scale, -scale, -scale),
-    V(scale, -scale, -scale),
-    V(-scale, -scale, scale),
-    V(scale, -scale, scale),
-    V(scale, scale, scale),
-    V(-scale, scale, scale),
-    V(-scale, scale, -scale),
-    V(scale, scale, -scale)
-]
-pyramid = [
-    V(0, scale, 0),
-    V(-scale, -scale, -scale),
-    V(scale, -scale, -scale),
-    V(-scale, -scale, scale),
-    V(scale, -scale, scale),
-]
+def parse_obj_model(path: str) -> List[V]:
+    with open(path, 'r') as object:
+        data = object.read().replace('\n\n', '\n')
+        # print(data.split('\n'))
+        vertexes = []
+        for line in data.split('\n'):
+            if len(line) == 0:
+                continue
+            elif line[0] == 'v' and line[1] == ' ':
+                params = line.split(' ')
+                # print(params)
+                vertexes.append(V(float(params[1]), float(params[2]), float(params[3])))
+        return vertexes
 
-for vertex in cube:
-    vertex.move(-separation, 0, 0)
-
-for vertex in pyramid:
-    vertex.move(separation, 0, 0)
+model = parse_obj_model('model.obj')
 
 speed = 1
 # theta = .005
@@ -133,31 +129,21 @@ while running:
     delta = clock.tick(30)
     screen.fill((0, 0, 0))
     screen.blit(orthogonal_text, orthogonal_text_rect)
-    screen.blit(orthogonal_matrix_image, (screen_size[0]//2 - orthogonal_matrix_image.get_width()//2, 9*screen_size[1]//12))
+    screen.blit(solovertices_text, solovertices_text_rect)
+    # screen.blit(orthogonal_matrix_image, (screen_size[0]//2 - orthogonal_matrix_image.get_width()//2, 9*screen_size[1]//12))
 
-    for vertex_1 in cube:
-        for vertex_2 in cube:
-            if vertex_1 == vertex_2:
-                continue
-            coordinates_1 = vertex_1.project(camera)
-            coordinates_2 = vertex_2.project(camera)
-            if coordinates_1 == (0, 0) or\
-               coordinates_2 == (0, 0):
-                continue
-            pygame.draw.line(screen, (255, 255, 255), coordinates_1, coordinates_2)
+    # for vertex_1 in model:
+    #     for vertex_2 in model:
+    #         if vertex_1 == vertex_2:
+    #             continue
+    #         coordinates_1 = vertex_1.project(camera)
+    #         coordinates_2 = vertex_2.project(camera)
+    #         if coordinates_1 == (0, 0) or\
+    #            coordinates_2 == (0, 0):
+    #             continue
+    #         pygame.draw.line(screen, (255, 255, 255), coordinates_1, coordinates_2)
 
-    for vertex_1 in pyramid:
-        for vertex_2 in pyramid:
-            if vertex_1 == vertex_2:
-                continue
-            coordinates_1 = vertex_1.project(camera)
-            coordinates_2 = vertex_2.project(camera)
-            if coordinates_1 == (0, 0) or\
-               coordinates_2 == (0, 0):
-                continue
-            pygame.draw.line(screen, (255, 255, 255), coordinates_1, coordinates_2)
-
-    for vertex in sorted(cube + pyramid, key=lambda v: v.brightness(camera)):
+    for vertex in sorted(model, key=lambda v: v.brightness(camera)):
         vertex.render(camera)
 
     pygame.display.flip()
@@ -185,13 +171,8 @@ while running:
        (keys[pygame.K_RSHIFT]):
         camera[1] -= speed*delta
 
-    cube = list(map(lambda vertex: V(
+    model = list(map(lambda vertex: V(
             vertex.z*sin(theta) + vertex.x*cos(theta),
             vertex.y,
             vertex.z*cos(theta) - vertex.x*sin(theta)
-        ), cube))
-    pyramid = list(map(lambda vertex: V(
-            vertex.z*sin(theta) + vertex.x*cos(theta),
-            vertex.y,
-            vertex.z*cos(theta) - vertex.x*sin(theta)
-        ), pyramid))
+        ), model))
