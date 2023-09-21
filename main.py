55774ba4cf52ae38d80b5ee057fb9ab2e3900a44
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2023-09-21 18:57:35 -0500

    camera rotations implemented

--- main.py
+++ main.py
@@ -18,13 +18,21 @@ def cartesian_to_pygame(
     return (x + screen_size[0]//2,
             screen_size[1]//2 - y)
 
+@dataclass
+class Camera(object):
+    x: float
+    y: float
+    z: float
+    theta_x: float = 0
+    theta_y: float = 0
+
 FOV = 1000
 
 screen_size = (1280, 720)
 view_distance = 640*8
 vertex_size = 2
 
-camera = [0, 15, -50]
+camera = Camera(0, 10, -35)
 scale = 100
 separation = 200
 
@@ -34,16 +42,6 @@ pygame.display.set_caption('obj')
 
 # CMUSerif = pygame.font.Font("cmunbx.ttf", 32)
 CMUSerif = pygame.font.Font("cmunbx.ttf", 64)
-orthogonal_text = CMUSerif.render("Importación de archivos OBJ", True, (255, 255, 255))
-solovertices_text = CMUSerif.render("(vértices y caras)", True, (255, 255, 255))
-orthogonal_text_rect = orthogonal_text.get_rect()
-orthogonal_text_rect.center = (screen_size[0]//2, screen_size[1]//12)
-solovertices_text_rect = solovertices_text.get_rect()
-solovertices_text_rect.center = (screen_size[0]//2, screen_size[1]//5)
-
-# orthogonal_matrix_image = pygame.image.load("lerp.png")
-# orthogonal_matrix_image = pygame.transform.scale(orthogonal_matrix_image, (orthogonal_matrix_image.get_width()*9//10,
-#                                                                            orthogonal_matrix_image.get_height()*9//10))
 
 screen = pygame.display.set_mode(screen_size)
 
@@ -62,31 +60,31 @@ class V(object):
         self.z += z
 
     def brightness(self,
-                   camera: List[float]) -> float:
-        return min(max(0, (-255/view_distance)*(self.z-camera[2]) + 255), 255)
+                   camera: Camera) -> float:
+        return min(max(0, (-255/view_distance)*(self.z-camera.z) + 255), 255)
 
     def project(self,
-                camera: List[float]) -> Tuple[float, float]:
-        # ORTHOGONAL PROJECTION
-        # coordinates = (self.x - camera[0],
-        #                self.y - camera[1])
-
-        coordinates = (((self.x-camera[0]) * FOV) // (self.z-camera[2]),
-                       ((self.y-camera[1]) * FOV) // (self.z-camera[2]))
-
-        # coordinates = (((self.x - camera[0]) * ((self.z - camera[2])/self.z)) + camera[0],
-        #                ((self.y - camera[1]) * ((self.z - camera[2])/self.z)) + camera[1])
-
-        return cartesian_to_pygame(*coordinates)
+                camera: Camera) -> Tuple[float, float]:
+        x = (self.x - camera.x)
+        y = (self.y - camera.y)
+        z = (self.z - camera.z)
+        tx = camera.theta_y
+        ty = camera.theta_x
+        rotated = V(
+            z*sin(ty)*cos(tx) + y*sin(tx)*sin(ty) + x*cos(ty),
+            y*cos(tx) - z*sin(tx),
+            -x*sin(ty) + y*sin(tx)*cos(ty) + z*cos(tx)*cos(ty)
+        )
+        return cartesian_to_pygame((rotated.x * FOV) // rotated.z,
+                                   (rotated.y * FOV) // rotated.z)
 
     def render(self,
-               camera: List[float]) -> None:
-        if self.z <= camera[2]:
-            return (0, 0)
+               camera: Camera) -> None:
+        # if self.z <= camera.z:
+        #     return (0, 0)
         
-        size = min(max(0, (-vertex_size/view_distance)*(self.z-camera[2]) + vertex_size), vertex_size)
+        size = min(max(0, (-vertex_size/view_distance)*(self.z-camera.z) + vertex_size), vertex_size)
 
-        # print(self.project(camera), self.brightness(camera), size)
         pygame.draw.circle(screen,
                            (self.brightness(camera),
                             self.brightness(camera), 
@@ -100,7 +98,7 @@ class L(object):
     vertex_2: V
 
     def render(self,
-               camera: List[float]) -> None:
+               camera: Camera) -> None:
         pygame.draw.line(screen, (255, 255, 255),
                          self.vertex_1.project(camera),
                          self.vertex_2.project(camera))
@@ -111,7 +109,7 @@ class Model(object):
     lines: List[L] | None
 
     def render(self,
-               camera: List[float]) -> None:
+               camera: Camera) -> None:
         if self.lines:
             for line in self.lines:
                 line.render(camera)
@@ -122,17 +120,14 @@ class Model(object):
     def transform(self, f: Callable[[V], V]):
         self.vertices = list(map(f, self.vertices))
         if self.lines:
-            transformed_lines = []
-            for line in self.lines:
-                transformed_lines.append(L(f(line.vertex_1), f(line.vertex_2)))
-            self.lines = transformed_lines
+            self.lines = list(map(lambda l: L(f(l.vertex_1), f(l.vertex_2)), self.lines))
 
 @dataclass
 class Scene(object):
     models: List[Model]
 
     def render(self,
-               camera: List[int]) -> None:
+               camera: Camera) -> None:
         lines = reduce(lambda l1, l2: l1 + l2, map(lambda m: m.lines, self.models))
         if lines:
             for line in lines:
@@ -178,7 +173,7 @@ model = parse_obj_model('model.obj')
 scene = Scene([model])
 
 speed = .05
-theta = .015
+theta = .015*2
 
 running = True
 clock = pygame.time.Clock()
@@ -189,8 +184,6 @@ while running:
     delta = clock.tick(30)
     time += delta
     screen.fill((0, 0, 0))
-    screen.blit(orthogonal_text, orthogonal_text_rect)
-    screen.blit(solovertices_text, solovertices_text_rect)
 
     scene.render(camera)
 
@@ -202,26 +195,40 @@ while running:
         
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        camera[0] -= speed*delta
+        camera.x -= cos(camera.theta_x)*speed*delta
+        camera.z -= sin(camera.theta_x)*speed*delta
     if keys[pygame.K_RIGHT]:
-        camera[0] += speed*delta
+        camera.x += cos(camera.theta_x)*speed*delta
+        camera.z += sin(camera.theta_x)*speed*delta
     if keys[pygame.K_UP]:
-        camera[2] += speed*delta
+        camera.x -= sin(camera.theta_x)*speed*delta
+        camera.y += sin(camera.theta_y)*speed*delta
+        camera.z += cos(camera.theta_x)*speed*delta
     if keys[pygame.K_DOWN]:
-        camera[2] -= speed*delta
+        camera.x += sin(camera.theta_x)*speed*delta
+        camera.y -= sin(camera.theta_y)*speed*delta
+        camera.z -= cos(camera.theta_x)*speed*delta
+    # if keys[pygame.K_w]:
+    #     FOV += speed*delta
+    # if keys[pygame.K_s]:
+    #     FOV -= speed*delta
+    if keys[pygame.K_a]:
+        camera.theta_x += theta
+    if keys[pygame.K_d]:
+        camera.theta_x -= theta
     if keys[pygame.K_w]:
-        FOV += speed*delta
+        camera.theta_y += theta
     if keys[pygame.K_s]:
-        FOV -= speed*delta
+        camera.theta_y -= theta
     if keys[pygame.K_SPACE]:
-        camera[1] += speed*delta
+        camera.y += speed*delta
     if (keys[pygame.K_LSHIFT]) or \
        (keys[pygame.K_RSHIFT]):
-        camera[1] -= speed*delta
+        camera.y -= speed*delta
 
     scene.models[0].transform(lambda vertex: V(
             vertex.z*sin(theta) + vertex.x*cos(theta),
             vertex.y,
             vertex.z*cos(theta) - vertex.x*sin(theta)
         ))
-    camera[2] = 40*abs(sin(time/4000)) -50
+    # camera.z = 40*abs(sin(time/4000)) -50
